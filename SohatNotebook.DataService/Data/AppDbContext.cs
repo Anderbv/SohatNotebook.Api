fa3757ccf3002c4ddb01c24c083d99662a23f82f
AnderCodeTT <arodriguezv@soen.pe>
2025-03-24 18:03:55 -0500

    Feat(Agregar proyecto de DatabaseContext and packages)

---
+++ SohatNotebook.DataService/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SohatNotebook.DataService.Data
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        // al heredar de IdentitiyDbContext se crearan todas las tablas tablas requeridas en la base de datos
+        // que requieran Identity como los roles de usuarios de asp .net core
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
