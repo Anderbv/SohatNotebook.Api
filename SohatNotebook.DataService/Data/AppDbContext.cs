fa3fb2454db07fca8026c63058d4f6aad07cc6af
AnderCodeTT <arodriguezv@soen.pe>
2025-03-27 17:35:09 -0500

    Feat(Se agrego la entidad User y se configuro el startup para que utilize nuestro modelo de AppDbContext)

--- SohatNotebook.DataService/Data/AppDbContext.cs
+++ SohatNotebook.DataService/Data/AppDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SohatNotebook.Entities.DbSet;
 
 namespace SohatNotebook.DataService.Data
 {
-    public class AppDbContext: IdentityDbContext
+    public class AppDbContext : IdentityDbContext
     {
         // al heredar de IdentitiyDbContext se crearan todas las tablas tablas requeridas en la base de datos
         // que requieran Identity como los roles de usuarios de asp .net core
-
+        public virtual DbSet<User> Users { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
         {
             
